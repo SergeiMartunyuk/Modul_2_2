cfe70a9f13e8b6af884bc418bcd493716f214e03
SergeiMartunyuk <alessaralfinit@gmail.com>
2024-10-07 21:51:31 +0500

    Add files via upload

--- Условная конструкция.py
+++ Условная конструкция.py
@@ -3,7 +3,9 @@ second = int(input('Enter a number: '))
 third = int(input('Enter a number: '))
 if first == second == third:
     print(3)
-elif first == second and first != third or first != second and first == third:
+elif (first == second and first != third
+      or first != second and  first == third
+      or second == third and second != first):
     print(2)
 else:
     print(0)
